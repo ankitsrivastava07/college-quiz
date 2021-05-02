df2abd91e53730ae1328bf31c3264e83ee8f1ee9
qtpl0036 <ravish.rathi@qtsolv.com>
2021-05-02 13:53:45 +0530

    change

--- user-auth/src/main/java/users/configure/WebSecurityConfig.java
+++ user-auth/src/main/java/users/configure/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package users.configure;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("college-quiz/login").permitAll().anyRequest().authenticated().and()
+		http.authorizeRequests().antMatchers("/college-quiz/login").permitAll().anyRequest().authenticated().and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.formLogin();
